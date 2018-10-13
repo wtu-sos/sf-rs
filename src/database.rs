ac030c510e22dfa9ad17f37396d2b3c80abf87a3
wtu-sos <595388976@qq.com>
2018-10-13 22:01:39 +0800

    modify

---
+++ src/database.rs
@@ -0,0 +1,69 @@
+use mysql::{self, params};
+
+pub const MYSQL_CONN : &str = "mysql://root:111111@localhost:3306/snowflake";
+
+pub const SQL_CREATE_DATABASE: &str = r"CREATE DATABASE IF NOT EXISTS `snowflake` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;";
+
+pub const SQL_CREATE_TABLE_WOKER_ENV : &str = r"
+    CREATE TABLE IF NOT EXISTS `worker_env` (
+    id INT not null primary key,
+    is_busy tinyint not null,
+    begin_time Datetime
+    ) ENGINE=InnoDB DEFAULT CHARSET=utf8;";
+
+pub const SQL_UPDATE_INSERT_WORKER_ENV : &str = r"
+    insert into worker_env (id, begin_time, is_busy)
+    value (:id, now(), :is_busy) 
+    on duplicate key update is_busy=:is_busy;";
+
+pub const SQL_CREATE_TABLE_WOKER_NODE : &str = r"
+    CREATE TABLE IF NOT EXISTS `worker_node` (
+    worker_id int not null primary key, 
+    is_online bool not null, 
+    content varchar(128))
+    ENGINE=InnoDB DEFAULT CHARSET=utf8;";
+
+pub const SQL_INSERT_PAYMENT : &str = r"
+    INSERT INTO woker_node 
+        (worker_id, is_online, content) 
+    VALUES 
+        (:worker_id, :is_online, :content)";
+
+#[derive(Debug, PartialEq, Eq)]
+        pub struct WorkerNode {
+            worker_id: i32,
+            is_online: bool,
+            content: Option<String>,
+        }
+
+impl WorkerNode {
+    pub fn new(worker_id: i32, is_online: bool, content: Option<String>) -> Self {
+        WorkerNode {
+            worker_id,
+            is_online,
+            content,
+        }
+    }
+}
+
+struct Database {
+    pool: mysql::Pool,
+}
+
+impl Database {
+    pub fn new () -> Database {
+        let pool = mysql::Pool::new(MYSQL_CONN); 
+        match pool {
+            Ok(ref _r) => println!("exec ok"),
+            Err(ref e) => {
+                println!("{}", e.to_string());
+                return;
+            },
+        }
+
+        Database {
+            pool
+        }
+    }
+}
+
--- src/main.rs
+++ src/main.rs
@@ -1,40 +1,15 @@
 use mysql::{self, params};
 
-const MYSQL_CONN : &str = "mysql://root:111111@localhost:3306/snowflake";
+mod database;
 
-const SQL_CREATE_DATABASE: &str = r"CREATE DATABASE IF NOT EXISTS `snowflake` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;";
-
-const SQL_CREATE_TABLE_WOKER_ENV : &str = r"
-    CREATE TABLE IF NOT EXISTS `worker_env` (
-    id INT not null primary key,
-    is_busy tinyint not null,
-    begin_time Datetime
-    ) ENGINE=InnoDB DEFAULT CHARSET=utf8;";
-
-const SQL_UPDATE_INSERT_WORKER_ENV : &str = r"
-    insert into worker_env (id, begin_time, is_busy)
-    value (:id, now(), :is_busy) 
-    on duplicate key update is_busy=:is_busy;";
-
-const SQL_CREATE_TABLE_WOKER_NODE : &str = r"
-    CREATE TABLE IF NOT EXISTS `worker_node` (
-    worker_id int not null primary key, 
-    is_online bool not null, 
-    content varchar(128))
-    ENGINE=InnoDB DEFAULT CHARSET=utf8;";
-
-const SQL_INSERT_PAYMENT : &str = r"
-    INSERT INTO woker_node 
-        (worker_id, is_online, content) 
-    VALUES 
-        (:worker_id, :is_online, :content)";
-
-#[derive(Debug, PartialEq, Eq)]
-struct WorkerNode {
-    worker_id: i32,
-    is_online: bool,
-    content: Option<String>,
-}
+use self::database::{
+    WorkerNode, 
+    MYSQL_CONN,
+    SQL_CREATE_DATABASE,
+    SQL_CREATE_TABLE_WOKER_ENV,
+    SQL_CREATE_TABLE_WOKER_NODE,
+    SQL_UPDATE_INSERT_WORKER_ENV,
+};
 
 fn main() {
     let pool = mysql::Pool::new(MYSQL_CONN); 
@@ -72,11 +47,11 @@ fn main() {
         Err(e) => println!("{}", e.to_string()),
     }
     let nodes = vec![
-        WorkerNode { worker_id: 1, is_online: false, content: None },
-        WorkerNode { worker_id: 3, is_online: false, content: Some("foo".into()) },
-        WorkerNode { worker_id: 5, is_online: false, content: None },
-        WorkerNode { worker_id: 7, is_online: false, content: None },
-        WorkerNode { worker_id: 9, is_online: false, content: Some("bar".into()) },
+        WorkerNode::new( 1, false,  None ),
+        WorkerNode::new( 3, false, Some("foo".into()) ),
+        WorkerNode::new( 5, false, None ),
+        WorkerNode::new( 7, false, None ),
+        WorkerNode::new( 9, false, Some("bar".into()) ),
     ];
 
 //    for mut stmt in pool.prepare(SQL_INSERT_PAYMENT).into_iter() {
